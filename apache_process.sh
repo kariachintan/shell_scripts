6dfad562270c8511344cf57d933e8705d03cd509
kariachintan <kariachintan@gmail.com>
2017-10-26 16:48:18 -0400

    Update apache_process.sh

--- apache_process.sh
+++ apache_process.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
-echo "running apache2 relaunch at $(date)" >> /var/log/uwe-crontab/crons.log
-ps auxw | grep apache2 | grep -v grep > /dev/null
-
-if [ $? != 0 ]
-then
-        /etc/init.d/apache2 restart >> /var/log/uwe-crontab/crons.log
-        echo "apache relaunched at $(date)" >> /var/log/uwe-crontab/crons.log
-else 
-        echo "apache is running"  >> /var/log/uwe-crontab/crons.log
+#!/bin/bash
+export ctl=/home/programs/httpd-2.0.63/bin/apachectl
+if test -f $ctl; then
+ case "$1" in
+ start|restart|graceful|stop)
+ $ctl $1
+ if [ `ps auxwwww|grep httpd|grep -v grep|wc -l` -gt 0 ]
+ then
+ echo Apache is running.
+ else
+ echo Apache is not running.
+ fi
+ ;;
+ status)
+ if [ `ps auxwwww|grep httpd|grep -v grep|wc -l` -gt 0 ]
+ then
+ echo Apache is running.
+ else
+ echo Apache is not running.
+ fi
+ ;;
+ *)
+ echo "Usage: apache start|restart|graceful|stop"
+ esac
+else
+ echo Apache is not installed;
 fi
